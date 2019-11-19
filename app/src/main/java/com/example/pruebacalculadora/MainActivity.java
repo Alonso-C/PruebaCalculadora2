88b1a02dd4e84d278eed65fdf6ef84db89dbd642
Alonso-C <AlonsoA701@gmail.com>
2019-11-19 09:59:40 -0300

    Cambios

---
+++ app/src/main/java/com/example/pruebacalculadora/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.pruebacalculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    button btn1, btn2, btn3, btn4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
